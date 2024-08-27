37f279100759ba584230bbc0db433936136e1455
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-08-27 17:40:07 -0300

    feature: loaders to redirect when logged or not

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,25 +3,33 @@ import Home from "../pages/private/home";
 import Login from "../pages/public/login";
 import Register from "../pages/public/register";
 
-const loader = () => {
+const privatePagesLoader = () => {
     const token = localStorage.getItem("token");
     if (token) return null;
     return redirect("/");
 }
 
+const publicPagesLoader = () => {
+    const token = localStorage.getItem("token");
+    if (!token) return null;
+    return redirect("/home");
+}
+
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Login />
+        element: <Login />,
+        loader: publicPagesLoader
     },
     {
         path: "/register",
-        element: <Register />
+        element: <Register />,
+        loader: publicPagesLoader
     },
     {
         path: "/home",
         element: <Home />,
-        loader: loader
+        loader: privatePagesLoader
     }
 
 ])
