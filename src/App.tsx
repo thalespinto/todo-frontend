132e8b42f20391198f18a7bb1dde09cb0b8e6b25
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-08-28 14:04:35 -0300

    configuring queryClient

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import {router} from "./routes";
 import {RouterProvider} from "react-router-dom";
 import {createTheme, ThemeProvider} from "@mui/material";
 import {AuthProvider} from "./services/auth/AuthProvider.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const theme = createTheme();
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <>
         <ThemeProvider theme={theme}>
-            <AuthProvider>
-                <RouterProvider router={router} />
-            </AuthProvider>
+            <QueryClientProvider client={queryClient}>
+                <AuthProvider>
+                    <RouterProvider router={router} />
+                </AuthProvider>
+            </QueryClientProvider>
         </ThemeProvider>
 
     </>
