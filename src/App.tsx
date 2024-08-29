d620ad74aeecaf147f897be26ab6165facd99da3
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-08-29 13:52:14 -0300

    feature: AddTodoDialog

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {RouterProvider} from "react-router-dom";
 import {createTheme, ThemeProvider} from "@mui/material";
 import {AuthProvider} from "./services/auth/AuthProvider.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 const theme = createTheme();
 const queryClient = new QueryClient();
@@ -12,11 +14,13 @@ function App() {
   return (
     <>
         <ThemeProvider theme={theme}>
-            <QueryClientProvider client={queryClient}>
-                <AuthProvider>
-                    <RouterProvider router={router} />
-                </AuthProvider>
-            </QueryClientProvider>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <QueryClientProvider client={queryClient}>
+                    <AuthProvider>
+                        <RouterProvider router={router} />
+                    </AuthProvider>
+                </QueryClientProvider>
+            </LocalizationProvider>
         </ThemeProvider>
 
     </>
