bd3c08985f82edf158a13596886c99351b2e5683
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-08-29 13:56:01 -0300

    formating home

--- src/pages/private/home/index.tsx
+++ src/pages/private/home/index.tsx
@@ -1,11 +1,26 @@
 import TodoList from "./components/TodoList";
-import { Box } from "@mui/material";
+import {Box} from "@mui/material";
+import AddTodoFab from "./components/AddTodoFab";
+import LogoutButton from "./components/LogoutButton";
 
 const Home = () => {
+
     return (
-        <Box maxWidth={"600px"} margin={"auto"}>
-            <TodoList />
-        </Box>
+        <>
+            <Box
+                maxWidth={"600px"}
+                margin={"auto"}
+                display={"flex"}
+                flexDirection={"column"}
+                alignItems={"end"}
+                gap={"40px"}
+            >
+                <LogoutButton />
+                <TodoList />
+                <AddTodoFab />
+            </Box>
+        </>
+
     );
 };
 
