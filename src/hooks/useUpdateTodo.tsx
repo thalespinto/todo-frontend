60d31e2eb69f414d984d98ac7bfb103199544fc0
Thales Pinto <thales.moreira1998.2@gmail.com>
2024-09-04 11:47:44 -0300

    fix: should invalidate todos query on update

--- src/hooks/useUpdateTodo.tsx
+++ src/hooks/useUpdateTodo.tsx
@@ -1,4 +1,4 @@
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {TodoApi} from "../services/todo/todoApi.ts";
 import {TTodo} from "../types/todo.types.ts";
 import {ToastType, useToast} from "./useToast.tsx";
@@ -7,10 +7,12 @@ const todoApi = new TodoApi()
 
 export const useUpdateTodo = () => {
     const { showToast } = useToast()
+    const queryClient = useQueryClient();
 
     return useMutation({
         mutationFn: (todo: Partial<TTodo>) => todoApi.update(todo),
         onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["todos"]}).then()
             showToast({
                 title: "Edição efetuada.",
                 type: ToastType.SUCCESS
